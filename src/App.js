3ac250a6752c3f8a9d81ee470e2c69659026cae5
dekkersbd <79840065+dekkersbd@users.noreply.github.com>
2021-06-08 18:42:24 +0200

    Make first pokemon card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from "axios";
 import './App.css';
 
 function App() {
+    const [pokemon, setPokemon] = useState(null);
+    console.log("wat is de pokemon?", pokemon)
+
+
+    useEffect(()=>{
+    async function fetchPokemon () {
+        const response = await axios.get (
+            `https://pokeapi.co/api/v2/pokemon/jigglypuff`
+        )
+        console.log("dit is de pokemon naam:", response.data.name)
+        setPokemon(response.data.name)
+    }
+    fetchPokemon();
+    }, []);
+
+
   return (
     <div>
       Begin hier met de opdracht!
+
     </div>
   );
 }
